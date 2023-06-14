dcc73b79c3d271df63c66cd2967f844d1978af41
AtilaGuler <108232136+AtiGuler@users.noreply.github.com>
2023-06-14 17:11:38 +0300

    -

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,17 +3,18 @@ const {StatusCodes} = require('http-status-codes');
 const {BadRequestError} = require('../errors');
 
 const register = async (req, res, next) => {
-  const {firstName, lastName, username, password} = req.body;
-
   try {
-    if (!firstName || !lastName || !username || !password) {
-      throw new BadRequestError(
-        'Please provide first name, last name, username and password'
-      );
-    }
-
     const user = await User.create(req.body);
-    res.status(StatusCodes.CREATED).json(user);
+    const token = user.createJWT();
+
+    res.status(StatusCodes.CREATED).json({
+      user: {
+        firstName: user.getFirstName(),
+        lastName: user.lastName,
+        username: user.username,
+      },
+      token,
+    });
   } catch (error) {
     next(error);
   }
